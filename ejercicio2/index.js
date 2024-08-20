97c460401cd64c54f0710be5cac65407fcbdf59f
artemio <artemio.derkachev@gmail.com>
2024-08-20 17:31:39 -0300

    punto 3 de ejercicio 2

--- ejercicio2/index.js
+++ ejercicio2/index.js
@@ -32,4 +32,39 @@ async function limitado(id) {
     }
 }
 
-limitado(6)
+//limitado(6)
+
+// 3. Agregar un nuevo producto (product).
+
+async function addNewProduct() {
+
+    const url = "https://fakestoreapi.com/products"
+    const product = {
+        id: 236,
+        title: "Snickers Clasico",
+        price: 1150,
+        description: "Una rica mezcla de cacahuete tostado, caramelo cremoso y la mejor crema de caramelo, cubierta de delicado chocolate con leche.",
+        category: "Chocolates",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNwJVn43VN-_aGKMt35Ihrrh5WHXH-940tiw&s",
+        rating: {rate: 5.0, coun: 20}
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(product) 
+        })
+
+        if (!response.ok) {
+            throw new Error('Error', response.status)
+        }
+
+        const data = await response.json()
+        console.log(data)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+addNewProduct()
